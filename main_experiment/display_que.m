2c66b144f1ff53bcbb4b8042aa85a89a065e0665
José Manuel Migoya Bussolotti <52668755+migmanu@users.noreply.github.com>
2023-07-04 22:36:48 +0200

    Change 2 for -1

--- main_experiment/display_que.m
+++ main_experiment/display_que.m
@@ -10,19 +10,19 @@ function display_que()
     if expt.which_anaglyph == 1
         if expt.which_queue == 1
             DrawFormattedText(win.window, ['B\n \n \n'], win.centerX , win.centerY , [255 255 255]);
-        elseif expt.which_queue == 2
+        elseif expt.which_queue == -1
             DrawFormattedText(win.window, ['M \n \n \n'], win.centerX , win.centerY , [255 255 255]);
         end
     elseif expt.which_anaglyph == 2
         if expt.which_queue == 1
             DrawFormattedText(win.window, ['FIIIIIIILLLL IT \n \n \n'], centerX, centerY, [255 255 255]);
-        elseif expt.which_queue == 2
+        elseif expt.which_queue == -1
             DrawFormattedText(win.window, ['FIIIIIIILLLL IT \n \n \n'], centerX, centerY, [255 255 255]);
         end
     elseif expt.which_anaglyph == 3
         if expt.which_queue == 1
             DrawFormattedText(win.window, ['FIIIIIIILLLL IT \n \n \n'], centerX, centerY, [255 255 255]);
-        elseif expt.which_queue == 2
+        elseif expt.which_queue == -1
             DrawFormattedText(win.window, ['FIIIIIIILLLL IT \n \n \n'], centerX, centerY, [255 255 255]);
         end
     end
