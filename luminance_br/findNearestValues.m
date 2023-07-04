55c8d65bddbdc081b1730563b1a08e37ea27b6ab
José Manuel Migoya Bussolotti <52668755+migmanu@users.noreply.github.com>
2023-07-04 21:00:05 +0200

    Max for min min for max

--- luminance_br/findNearestValues.m
+++ luminance_br/findNearestValues.m
@@ -1,6 +1,6 @@
  function [max, min] = findNearestValues(vector)
     meanValue = mean(vector);  % Compute the mean of the vector
     [~, indices] = sort(abs(vector - meanValue));  % Sort the absolute differences from the mean
-	max = vector(indices(1));
-	min = vector(indices(2));
+	min = vector(indices(1));
+	max = vector(indices(2));
 end
