e9c11b03f66e4f118426c7b7201a6f882dd66007
José Manuel Migoya Bussolotti <52668755+migmanu@users.noreply.github.com>
2023-07-04 22:34:47 +0200

    Initial commit

---
+++ main_experiment/passive_view.m
@@ -0,0 +1,18 @@
+function passive_view()
+	global expt
+	counter = 0;
+	dominant = '';
+
+	while counter < 30
+		display_anaglyph();
+		while ~strcmp(dominant, 'LeftArrow') && ~strcmp(dominant, 'RightArrow') && ~strcmp(dominant, 'DownArrow')
+			% Wait for keypress
+			[dominant, wasKeyDown] = response(wasKeyDown);
+            expt.passive_view_arr(counter) = dominant;
+			end
+		display_cross();
+		pause(10);
+		display_anaglyph();
+		pause(0.75);
+	end
+end
