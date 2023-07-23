bf8c3f7361441c76a40babe580795d5fafd9b758
raxideandrade <raxideandrade.de@gmail.com>
2023-07-23 22:45:59 +0200

    changes

--- main_experiment/plot_percentage_sta.m
+++ main_experiment/plot_percentage_sta.m
@@ -1,7 +1,7 @@
 function plot_percentage_sta(percentage_condi_1, percentage_cond_2, percentage_cond_3)
     % Create a bar plot showing the percentages for all three conditions
     figure;
-    bar([percentage_condi_1, percentage_cond_2, percentage_cond_3],'FaceColor','r','b','g');
+    bar([percentage_condi_1, percentage_cond_2, percentage_cond_3],'r');
     xlabel('Conditions');
     ylabel('Percentage stability');
     title('Percentage of Most Repeated String');
--- main_experiment/run_secuence.m
+++ main_experiment/run_secuence.m
@@ -1,55 +1,55 @@
 function run_secuence(event)
-	global data
-	global expt
-	global win
-	
-	queue = event;
-	counter = 1;
-	dominant = '';
-	vividness = '';
-	wasKeyDown = false;
+global data
+global expt
+global win
 
-	assign_mock = (1:30)
-	assign_mock = assign_mock(randperm(length(assign_mock)))
-    expt.mock_arra = assign_mock
-	mock_counter = 1;
+queue = event;
+counter = 1;
+dominant = '';
+vividness = '';
+wasKeyDown = false;
 
-	while expt.isrunning && counter <= expt.block_size
-		if mod(assign_mock(mock_counter), 5) == 0 
-			expt.which_anaglyph = 2
-		else
-			expt.which_anaglyph = 1
-		end
-		mock_counter =  mock_counter + 1;
+assign_mock = (1:30)
+assign_mock = assign_mock(randperm(length(assign_mock)))
+expt.mock_arra = assign_mock
+mock_counter = 1;
 
-		display_anaglyph();
-		pause(0.75);
-		% Subject chooses dominant
-		while ~strcmp(dominant, 'LeftArrow') && ~strcmp(dominant, 'RightArrow') && ~strcmp(dominant, 'DownArrow')
-            display_cross
-			% Wait for keypress
-			[dominant, wasKeyDown] = response(wasKeyDown);
-		end
+while expt.isrunning && counter <= expt.block_size
+    if mod(assign_mock(mock_counter), 5) == 0
+        expt.which_anaglyph = 2
+    else
+        expt.which_anaglyph = 1
+    end
+    mock_counter =  mock_counter + 1;
 
-		display_frame();
-        pause(7);
+    display_anaglyph();
+    pause(0.75);
+    % Subject chooses dominant
+    while ~strcmp(dominant, 'LeftArrow') && ~strcmp(dominant, 'RightArrow') && ~strcmp(dominant, 'DownArrow')
+        display_cross
+        % Wait for keypress
+        [dominant, wasKeyDown] = response(wasKeyDown);
+    end
 
-        data(expt.trial).block_count = expt.block_count; 
-		data(expt.trial).queue = queue;
-		data(expt.trial).dominant = dominant;
-		data(expt.trial).which_anaglyph = expt.which_anaglyph;
-		% Subject rates vividness
-		while ~any(strcmp(vividness,{'1!','2@','3#','4$'}))
-			DrawFormattedText(win.window, ['Rate Vividness \n \n  1 = low 2 = moderate 3 = elevated 4 = high \n \n \n'], 'center', 'center', [255 255 255]);
-			Screen('Flip', win.window);%updates the screen 
-			[vividness, wasKeyDown] = response(wasKeyDown);
-            
-        end
-        data(expt.trial).vividness = vividness;
-		expt.trial = expt.trial + 1;
-		counter = counter + 1;
-        dominant = '';
-        vividness = '';
-        save(fullfile('output', ['BR_', num2str(expt.subject), '.mat']), 'data');
-	end
+    display_frame();
+    pause(7);
+
+    data(expt.trial).block_count = expt.block_count;
+    data(expt.trial).queue = queue;
+    data(expt.trial).dominant = dominant;
+    data(expt.trial).which_anaglyph = expt.which_anaglyph;
+    % Subject rates vividness
+    while ~any(strcmp(vividness,{'1!','2@','3#','4$'}))
+        DrawFormattedText(win.window, ['Rate Vividness \n \n  1 = low 2 = moderate 3 = elevated 4 = high \n \n \n'], 'center', 'center', [255 255 255]);
+        Screen('Flip', win.window);%updates the screen
+        [vividness, wasKeyDown] = response(wasKeyDown);
+
+    end
+    data(expt.trial).vividness = vividness;
+    expt.trial = expt.trial + 1;
+    counter = counter + 1;
+    dominant = '';
+    vividness = '';
+    save(fullfile('output', ['BR_', num2str(expt.subject), '.mat']), 'data');
+end
 end
